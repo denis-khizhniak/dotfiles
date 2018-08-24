88d96fd11ce0cde3dd19fef2417b4f26603f2e1a
Denis Khizhnyak <4denis.kh@gmail.com>
2018-08-24 17:06:53 +0300

    huge modifications in dotfiles project structure and contents

--- dzen/calendar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Simple calendar for dzen2
-#
-
-FONT="-*-monospace-*-*-*-*-*-*-*-*-*-*-*-*"
-
-echo "$(cal -wm)" | dzen2 -p -x "500" -y "30" -w "240" -l 7 -sa 'c' -ta 'c' -fn "$FONT"\
-                    -title-name 'popup_calendar' -e 'onstart=uncollapse;button1=exit;button3=exit'
